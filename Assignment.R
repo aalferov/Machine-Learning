7fad136cf22927e9e5bf6b3e2e926793481c14ee
Andrey Alferov <aalferov@gmail.com>
2015-06-21 12:35:58 +0400

    Version 2

--- Assignment.R
+++ Assignment.R
@@ -1,7 +1,10 @@
 library(caret)
+library(randomForest)
+library(rpart)
+library(rpart.plot)
 
 ## Download files with training and test data & load them in data frames
-setwd("D:/Coursera/Data Science/Machine Learning")
+setwd("D:/Coursera/Data Science/Machine-Learning")
 
 fileUrl <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
 fileName <- "pml-training.csv"
@@ -11,7 +14,7 @@ if(!file.exists(fileName))
   download.file(fileUrl, fileName)
 }
 
-trainingDS <- read.csv(fileName, stringsAsFactors = FALSE)
+trainingDS <- read.csv(fileName, na.strings=c("NA","#DIV/0!", ""))
 
 
 fileUrl <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
@@ -22,31 +25,49 @@ if(!file.exists(fileName))
   download.file(fileUrl, fileName)
 }
 
-testingDS <- read.csv(fileName, stringsAsFactors = FALSE)
+testingDS <- read.csv(fileName, na.strings=c("NA","#DIV/0!", ""))
 
 ## Clean up training dataset
 
-dim(trainingDS) #So we have 19622 observations of 160 variables
+dim(trainingDS) 
+dim(testingDS) 
+#So we have 19622 observations of 160 variables in training dataset and 20 observations of 20 variables in testing dataset
 
 summary(trainingDS)
 
-#Let's remove variables with NA's
-trainingDS <-trainingDS[,colSums(is.na(trainingDS)) == 0]
-
-#Let's remove variables "X", "raw_timestamp_part_1", "raw_timestamp_part_2" and "cvtd_timestamp" as they shouldn't be influantional on predicted result by their nature 
-trainingDS <- subset(trainingDS, select = -c(X, raw_timestamp_part_1, raw_timestamp_part_2, cvtd_timestamp))
+#Remove variables "user_name", "X", "raw_timestamp_part_1", "raw_timestamp_part_2" and "cvtd_timestamp" as they shouldn't be influantional on predicted result by their nature 
+trainingDS <- subset(trainingDS, select = -c(user_name, X, raw_timestamp_part_1, raw_timestamp_part_2, cvtd_timestamp))
+testingDS <- subset(testingDS, select = -c(user_name, X, raw_timestamp_part_1, raw_timestamp_part_2, cvtd_timestamp))
 
 str(trainingDS)
 
-#TODO: remove variables with "", "#DIV/0!" values
-
-## Remove factor variables with a single value as they have no influence on prediction
-#trainingDS <- subset(trainingDS, select = -c(kurtosis_yaw_belt, skewness_yaw_belt, kurtosis_yaw_dumbbell, skewness_yaw_dumbbell,
-                                         amplitude_yaw_dumbbell, kurtosis_yaw_forearm, skewness_yaw_forearm, amplitude_yaw_forearm))
+#Remove variables with NA's
+trainingDS <-trainingDS[ ,colSums(is.na(trainingDS)) == 0]
+testingDS <-testingDS[ ,colSums(is.na(testingDS)) == 0]
 
 str(trainingDS)
 
-ctrl <- rfeControl(functions = lmFuncs, method = "repeatedcv", repeats = 5, verbose = FALSE)
-lmProfile <- rfe(subset(trainingDS, -classe), trainingDS$classe, sizes = subsets, rfeControl = ctrl)
+#Exclude predictors with near zero variance as they doesn't affect prediction result
+nzvCol <- nearZeroVar(trainingDS)
+trainingDS <- trainingDS[ ,-nzvCol]
 
+#Create data partitions
+set.seed(210615)
+trainset <- createDataPartition(trainingDS$classe, p = 0.8, list = FALSE)
+training <- trainingDS[trainset, ]
+validation <- trainingDS[-trainset, ]
 
+#Train first model via Decision Tree algorythm
+modelDT <- rpart(classe ~ ., data = training, method = "class")
+
+#Make a prediction
+predictionDT <- predict(modelDT, validation, type = "class")
+
+#Plot the decision tree
+rpart.plot(modelDT, main = "Classification Tree", extra = 102, under = TRUE, faclen = 0)
+
+#Build the Confusion Matrix to check the result of prediction
+confusionMatrix(predictionDT, validation$classe)
+
+#Train second model via Random Forest algorythm
+modelRF <- randomForest(classe ~. , data = training, method = "class")
